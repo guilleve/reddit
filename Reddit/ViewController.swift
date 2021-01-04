8044398bca725a366f1858f2285c975726231750
gvergara <guillermovergara13@gmail.com>
2021-01-04 11:11:00 -0300

    Empty project creation

---
+++ Reddit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reddit
+//
+//  Created by Guillermo Vergara on 1/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
